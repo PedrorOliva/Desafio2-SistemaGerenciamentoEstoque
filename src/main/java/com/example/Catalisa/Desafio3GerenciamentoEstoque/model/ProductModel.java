eae10d4ed73e5736d8bde6f169b42ffdd961a67d
PedrorOliva <pedro.robson@zup.com.br>
2023-08-31 11:18:09 -0300

    feat: adicionando model

---
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/model/ProductModel.java
@@ -0,0 +1,50 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque.model;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table(name = "tb_product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank(message = "O nome é obrigatório!!")
+  @Length(min = 3, max = 50, message = "O nome deverá ter no máximo {max} caracteres.")
+  private String name;
+
+  @NotBlank(message = "A descrição é obrigatória!!")
+  @Length(min = 10, max = 255, message = "{max} é o máximo de caracteres.")
+  @Column(nullable = false)
+  private String description;
+
+  @Column(name = "price", nullable = false)
+  @NotNull(message = "O preço é obrigatório!")
+  private Double price;
+
+  @PositiveOrZero
+  private Integer amount = 0;
+
+  @NotBlank(message = "A categoria é obrigatória!!")
+  private String category;
+
+  private String operation;
+
+  private Integer operationAmount = 0;
+
+  public ProductModel(long id, String name, String descrption, Double price, Integer amount, String category) {
+  }
+
+}
