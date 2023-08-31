969b854ac84e3a8fe1e83af8005ffd2d07352c82
PedrorOliva <pedro.robson@zup.com.br>
2023-08-31 11:17:56 -0300

    feat: subindo projeto

---
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/Desafio3GerenciamentoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3GerenciamentoEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3GerenciamentoEstoqueApplication.class, args);
+	}
+
+}
