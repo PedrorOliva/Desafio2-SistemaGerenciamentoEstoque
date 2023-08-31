fb7c7ec6067e276984274d697ff97576397f75f6
PedrorOliva <pedro.robson@zup.com.br>
2023-08-31 11:19:56 -0300

    feat: adicionando exception handler

---
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/exceptions/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.Objects;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+  @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleFieldValidation(MethodArgumentNotValidException e) {
+    String errorMessage = Objects.requireNonNull(e.getFieldError()).getDefaultMessage();
+    return ResponseEntity.badRequest().body(errorMessage);
+  }
+
+
+}
