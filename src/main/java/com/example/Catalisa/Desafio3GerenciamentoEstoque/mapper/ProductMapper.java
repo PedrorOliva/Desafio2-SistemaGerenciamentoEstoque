55082271a66ff5d7b6e431fc821de6990405f108
PedrorOliva <pedro.robson@zup.com.br>
2023-08-31 11:19:39 -0300

    feat: adicionando dto e mapper

---
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque.mapper;
+
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.dto.ProductDTO;
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.model.ProductModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+  public ProductDTO toProductDTO(ProductModel productModel){
+    ProductDTO productDTO = new ProductDTO();
+
+    productDTO.setName(productModel.getName());
+    productDTO.setDescription(productModel.getDescription());
+    productDTO.setPrice(productModel.getPrice());
+    productDTO.setAmount(productModel.getAmount());
+    productDTO.setCategory(productModel.getCategory());
+
+    return productDTO;
+  }
+}
