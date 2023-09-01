b8a17107836341d704e4768ee628b3411736e78f
PedrorOliva <pedro.robson@zup.com.br>
2023-09-01 10:02:28 -0300

    refac: refatorando ProductService

--- src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/service/ProductService.java
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/service/ProductService.java
@@ -1,12 +1,17 @@
 package com.example.Catalisa.Desafio3GerenciamentoEstoque.service;
 
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.dto.ProductDTO;
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.exceptions.handleCategoryNotFound;
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.exceptions.handleIdNotFound;
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.exceptions.handleNameNotFound;
+import com.example.Catalisa.Desafio3GerenciamentoEstoque.mapper.ProductMapper;
 import com.example.Catalisa.Desafio3GerenciamentoEstoque.model.ProductModel;
 import com.example.Catalisa.Desafio3GerenciamentoEstoque.model.factory.ProductFactory;
 import com.example.Catalisa.Desafio3GerenciamentoEstoque.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,63 +20,106 @@ public class ProductService {
   @Autowired
   ProductRepository productRepository;
 
-  public ProductModel create(ProductModel productModel, ProductFactory productFactory) {
+  @Autowired
+  ProductMapper productMapper;
+
+  public ProductDTO create(ProductModel productModel, ProductFactory productFactory) {
     Integer amount = productFactory.operation(productModel.getOperation())
         .score(productModel.getAmount(), productModel.getOperationAmount());
     productModel.setAmount(amount);
     productModel.setAmount(productModel.getAmount());
-    return productRepository.save(productModel);
+    productRepository.save(productModel);
+    ProductDTO productDTO = productMapper.toProductDTO(productModel);
+    return productDTO;
   }
 
-  public List<ProductModel> findAllProducts() {
-    return productRepository.findAll();
-  }
+  public List<ProductDTO> listAllProducts() {
+    List<ProductModel> productModels = productRepository.findAll();
+    List<ProductDTO> productDTOS = new ArrayList<>();
 
-  public Optional<ProductModel> findOneById(Long id) {
-
-    return productRepository.findById(id);
-  }
-
-  public Optional<ProductModel> findOneByName(String name) {
-    return productRepository.findByName(name);
-  }
-
-  public List<ProductModel> findProductByCategory(String category) {
-    return productRepository.findByCategory(category);
-  }
-
-  public ProductModel updateProduct(Long id, ProductModel productModel) {
-    ProductModel product = findOneById(id).get();
-
-    if (productModel.getName() != null) {
-      product.setName(productModel.getName());
+    for (ProductModel productModel : productModels) {
+      productDTOS.add(productMapper.toProductDTO(productModel));
     }
-    if (productModel.getDescription() != null) {
-      product.setDescription(productModel.getDescription());
-    }
-    if (productModel.getPrice() != null) {
-      product.setPrice(productModel.getPrice());
-    }
-    if (productModel.getAmount() != null) {
-      product.setAmount(productModel.getAmount() + product.getAmount());
-    }
-
-    return productRepository.save(product);
+    return productDTOS;
   }
 
-  public ProductModel exitProduct(Long id, ProductModel productModel, ProductFactory productFactory) {
-    ProductModel product = findOneById(id).get();
+  public Optional<ProductDTO> findById(Long id) {
+    Optional<ProductModel> productModel = productRepository.findById(id);
+    if (productModel.isEmpty()) {
+      throw new handleIdNotFound("ID not found!");
+    }
+    ProductModel product = productModel.get();
+    ProductDTO productDTO = productMapper.toProductDTO(product);
+    return Optional.of(productDTO);
+  }
 
-      Integer newAmount = productFactory.operation(product.getOperation()).score(product.getAmount(),
+  public List<ProductDTO> findByName(String name) {
+    List<ProductModel> productModels = productRepository.findByName(name);
+    if (productModels.isEmpty()) {
+      throw new handleNameNotFound("Name not found!");
+    }
+    List<ProductDTO> product = new ArrayList<>();
+    for (ProductModel productModel : productModels) {
+      product.add(productMapper.toProductDTO(productModel));
+    }
+    return product;
+  }
+
+  public List<ProductDTO> findByCategory(String category) {
+    List<ProductModel> productModels = productRepository.findByCategory(category);
+    if (productModels.isEmpty()) {
+      throw new handleCategoryNotFound("Category not found!");
+    }
+    List<ProductDTO> product = new ArrayList<>();
+    for (ProductModel productModel : productModels) {
+      product.add(productMapper.toProductDTO(productModel));
+    }
+    return product;
+  }
+
+  public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
+    Optional<ProductModel> productOptional = productRepository.findById(id);
+    if (productOptional.isEmpty()) {
+      throw new RuntimeException("ID not found!");
+    }
+
+    ProductModel product = productOptional.get();
+    if (productDTO.getName() != null) {
+      product.setName(productDTO.getName());
+    }
+    if (productDTO.getDescription() != null) {
+      product.setDescription(productDTO.getDescription());
+    }
+    if (productDTO.getPrice() != null) {
+      product.setPrice(productDTO.getPrice());
+    }
+    if (productDTO.getAmount() != null) {
+      product.setAmount(productDTO.getAmount() + product.getAmount());
+    }
+    productRepository.save(product);
+    return productMapper.toProductDTO(product);
+  }
+
+  public ProductDTO exitProduct(Long id, ProductModel productModel, ProductFactory productFactory) {
+    Optional<ProductModel> productOptional = productRepository.findById(id);
+    if (productOptional.isEmpty()) {
+      throw new RuntimeException("ID not found!");
+    }
+    ProductModel product = productOptional.get();
+    Integer newAmount = productFactory.operation(productModel.getOperation()).score(product.getAmount(),
           productModel.getOperationAmount());
-      product.setOperationAmount(productModel.getOperationAmount());
-      product.setAmount(newAmount);
-      product.setOperation(productModel.getOperation());
+    product.setOperationAmount(productModel.getOperationAmount());
+    product.setAmount(newAmount);
+    product.setOperation(productModel.getOperation());
 
-      return productRepository.save(product);
+    productRepository.save(product);
+    return productMapper.toProductDTO(product);
+
   }
 
-  public void deleteProduct(Long id) {
+  public void delete(Long id) {
+    Optional<ProductModel> productModel = productRepository.findById(id);
+
     productRepository.deleteById(id);
   }
 
