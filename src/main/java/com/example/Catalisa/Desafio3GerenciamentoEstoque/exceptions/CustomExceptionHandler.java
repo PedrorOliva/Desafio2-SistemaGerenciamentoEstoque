f974d04f357bb9930aff53f6bc82ba027cea3d53
PedrorOliva <pedro.robson@zup.com.br>
2023-09-01 10:04:21 -0300

    feat: add customExceptionHandler, handleCategory, handleName e handleId

---
+++ src/main/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+  @org.springframework.web.bind.annotation.ExceptionHandler(handleIdNotFound.class)
+  public ResponseEntity<Object> handleIdNotFound(handleIdNotFound e) {
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", new Date());
+    body.put("message", e.getMessage());
+
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+  }
+
+  @org.springframework.web.bind.annotation.ExceptionHandler(handleCategoryNotFound.class)
+  public ResponseEntity<Object> handleIdNotFound(handleCategoryNotFound e) {
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", new Date());
+    body.put("message", e.getMessage());
+
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+  }
+
+  @org.springframework.web.bind.annotation.ExceptionHandler(handleNameNotFound.class)
+  public ResponseEntity<Object> handleIdNotFound(handleNameNotFound e) {
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", new Date());
+    body.put("message", e.getMessage());
+
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+  }
+}
---
+++ src/test/java/com/example/Catalisa/Desafio3GerenciamentoEstoque/Desafio3GerenciamentoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Catalisa.Desafio3GerenciamentoEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3GerenciamentoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
